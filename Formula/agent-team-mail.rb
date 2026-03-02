2ee329292ebbc1743ca1079cc64dc754747a06cc
Rand Lee <github@randlee.com>
2026-03-01 19:36:19 -0800

    Update agent-team-mail to v0.27.0

--- Formula/agent-team-mail.rb
+++ Formula/agent-team-mail.rb
@@ -4,13 +4,13 @@
 class AgentTeamMail < Formula
   desc "CLI and daemon for mail-like messaging with Claude agent teams"
   homepage "https://github.com/randlee/agent-team-mail"
-  version "0.25.0"
+  version "0.27.0"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/randlee/agent-team-mail/releases/download/v0.25.0/atm_0.25.0_x86_64-apple-darwin.tar.gz"
-      sha256 "5c6209f4bc0d907a47a938162655c8d079ee71256e75cd394cd98b02c940a7d8"
+      url "https://github.com/randlee/agent-team-mail/releases/download/v0.27.0/atm_0.27.0_x86_64-apple-darwin.tar.gz"
+      sha256 "2c05d2c959b815e69e679bbf1069439f8131d47ce7fc71fca1e972bd0152c092"
 
       def install
         bin.install "atm"
@@ -20,8 +20,8 @@ class AgentTeamMail < Formula
       end
     end
     on_arm do
-      url "https://github.com/randlee/agent-team-mail/releases/download/v0.25.0/atm_0.25.0_aarch64-apple-darwin.tar.gz"
-      sha256 "2ed98e5998119ee163432ad2abe2fd0e56652ca117fc5162af258dfc69db9c1b"
+      url "https://github.com/randlee/agent-team-mail/releases/download/v0.27.0/atm_0.27.0_aarch64-apple-darwin.tar.gz"
+      sha256 "7d6999ac53187e31ede5fa502ecf03984256ea71e3cf6fc2c739a6d4724a9402"
 
       def install
         bin.install "atm"
@@ -35,8 +35,8 @@ class AgentTeamMail < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/randlee/agent-team-mail/releases/download/v0.25.0/atm_0.25.0_x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "ecb60b60b8bd0059a2b94102f89e61042d613dbdc8d593dd68aaf9d6d0d01298"
+        url "https://github.com/randlee/agent-team-mail/releases/download/v0.27.0/atm_0.27.0_x86_64-unknown-linux-gnu.tar.gz"
+        sha256 "be828dfb96cbeef6d0da227ba828a4679ea47265dcdf410121293776eeeac5e5"
 
         def install
           bin.install "atm"
