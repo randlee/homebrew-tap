a532d870c37ef2d10ab8983334693022749b45bb
Rand Lee <randlee@users.noreply.github.com>
2026-03-08 17:04:03 -0700

    chore: bump agent-team-mail + atm to v0.41.1

--- Formula/atm.rb
+++ Formula/atm.rb
@@ -4,13 +4,13 @@
 class Atm < Formula
   desc "CLI and daemon for mail-like messaging with Claude agent teams"
   homepage "https://github.com/randlee/agent-team-mail"
-  version "0.38.0"
+  version "0.41.1"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/randlee/agent-team-mail/releases/download/v0.38.0/atm_0.38.0_x86_64-apple-darwin.tar.gz"
-      sha256 "9c4cc97ad2051f36273aab94f3b08cba6ed6a68d00fef6b408b706460d10554c"
+      url "https://github.com/randlee/agent-team-mail/releases/download/v0.41.1/atm_0.41.1_x86_64-apple-darwin.tar.gz"
+      sha256 "bd727e8291fa6c83af08a5d7b28bf2ae6a214f0d608d1723ddd40995782aeb24"
 
       def install
         bin.install "atm"
@@ -20,8 +20,8 @@ class Atm < Formula
       end
     end
     on_arm do
-      url "https://github.com/randlee/agent-team-mail/releases/download/v0.38.0/atm_0.38.0_aarch64-apple-darwin.tar.gz"
-      sha256 "104027995ec794fd2062b129ef189508c31ac39c84b23314674acbadbc8ffbda"
+      url "https://github.com/randlee/agent-team-mail/releases/download/v0.41.1/atm_0.41.1_aarch64-apple-darwin.tar.gz"
+      sha256 "14bf39929433faea3dc5e1d6c752b920e1bf6af32224e33b2e4ed153138d0216"
 
       def install
         bin.install "atm"
@@ -35,8 +35,8 @@ class Atm < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/randlee/agent-team-mail/releases/download/v0.38.0/atm_0.38.0_x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "e423b414b7b47274dab88ea88e37620ce99c442cc4a227a6db5a6ec93a1bbb4d"
+        url "https://github.com/randlee/agent-team-mail/releases/download/v0.41.1/atm_0.41.1_x86_64-unknown-linux-gnu.tar.gz"
+        sha256 "ca7f9615ff333c01934ddccf67112e1883a24d3fbd9af8b67ea5a439c775c059"
 
         def install
           bin.install "atm"
