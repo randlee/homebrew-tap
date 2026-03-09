d7c19d8f478cace9f925f592175effe4837f7ae2
Rand Lee <github@randlee.com>
2026-03-08 22:53:36 -0700

    Update agent-team-mail to v0.42.1

--- Formula/agent-team-mail.rb
+++ Formula/agent-team-mail.rb
@@ -4,30 +4,32 @@
 class AgentTeamMail < Formula
   desc "CLI and daemon for mail-like messaging with Claude agent teams"
   homepage "https://github.com/randlee/agent-team-mail"
-  version "0.41.1"
+  version "0.42.1"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/randlee/agent-team-mail/releases/download/v0.41.1/atm_0.41.1_x86_64-apple-darwin.tar.gz"
-      sha256 "bd727e8291fa6c83af08a5d7b28bf2ae6a214f0d608d1723ddd40995782aeb24"
+      url "https://github.com/randlee/agent-team-mail/releases/download/v0.42.1/atm_0.42.1_x86_64-apple-darwin.tar.gz"
+      sha256 "897695c3d778e266920a2109241d1fe92392089bea2a1455682c9e0f263df90d"
 
       def install
         bin.install "atm"
         bin.install "atm-daemon"
         bin.install "atm-agent-mcp"
         bin.install "atm-tui"
+        bin.install "sc-compose"
       end
     end
     on_arm do
-      url "https://github.com/randlee/agent-team-mail/releases/download/v0.41.1/atm_0.41.1_aarch64-apple-darwin.tar.gz"
-      sha256 "14bf39929433faea3dc5e1d6c752b920e1bf6af32224e33b2e4ed153138d0216"
+      url "https://github.com/randlee/agent-team-mail/releases/download/v0.42.1/atm_0.42.1_aarch64-apple-darwin.tar.gz"
+      sha256 "3b4aac25980e41b966766c01a6a883dfed2e2b3187a5e92ef9e77aa009fd6802"
 
       def install
         bin.install "atm"
         bin.install "atm-daemon"
         bin.install "atm-agent-mcp"
         bin.install "atm-tui"
+        bin.install "sc-compose"
       end
     end
   end
@@ -35,14 +37,15 @@ class AgentTeamMail < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/randlee/agent-team-mail/releases/download/v0.41.1/atm_0.41.1_x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "ca7f9615ff333c01934ddccf67112e1883a24d3fbd9af8b67ea5a439c775c059"
+        url "https://github.com/randlee/agent-team-mail/releases/download/v0.42.1/atm_0.42.1_x86_64-unknown-linux-gnu.tar.gz"
+        sha256 "397d76f8dd0604a308c026022a645533f71103488297038b8c5b1d07e5c4ea55"
 
         def install
           bin.install "atm"
           bin.install "atm-daemon"
           bin.install "atm-agent-mcp"
           bin.install "atm-tui"
+          bin.install "sc-compose"
         end
       end
     end
